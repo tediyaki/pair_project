f04051f77fbb3aa45cadca5a069a3f01b1058275
havus <havus.it@gmail.com>
2019-07-31 16:36:07 +0700

    11

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 const UserController = require('../controllers/user-controller');
 const RepairmanController = require('../controllers/repairman-controller');
@@ -11,9 +11,7 @@ router.get('/login', UserController.showLoginPage);
 router.post('/login', UserController.loginUser);
 
 
-// router.get('/history', UserController)
-router.get('/testInput', UserController.showHistory);
-router.get('/testInput', UserController.findAVG);
+// router.get('/testInput', UserController.findAVG);
 router.get('/:username/dashboard', UserController.showHistory);
 router.get('/:username/edit', UserController.showEditForm);
 router.post('/:username/edit', UserController.updateUser);
@@ -27,10 +25,7 @@ router.post('/:username/repairman', (req, res) => {
 });
 
 
-<<<<<<< HEAD
-router.get('/testInput', UserController.bookRepairman)
-router.get('/testInput2', UserController.giveRating)
-=======
->>>>>>> a0c19ced0c2cadafcfce37b63fff9c680efcfae0
+router.get('/testInput', UserController.bookRepairman);
+router.get('/testInput2', UserController.giveRating);
 
-module.exports = router
+module.exports = router;
