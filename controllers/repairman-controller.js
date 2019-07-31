f229613a3f648541036de4ed2662c20a20d06a3a
havus <havus.it@gmail.com>
2019-07-31 11:39:45 +0700

    2

--- controllers/repairman-controller.js
+++ controllers/repairman-controller.js
@@ -1,14 +1,29 @@
 const repairmanModel = require('../models').Repairman;
+const {or, and, gt, lt} = require('../models').sequelize.Sequelize.Op
 
 class Repairman {
   static findAll(req, res) {
     repairmanModel.findAll()
     .then((data) => {
-      res.send(data);
+      // res.send(data);
+      res.render('repairman', {repairman: data});
     })
     .catch((err) => {
       res.send(err);
     })
+  }
+
+  static findSome(req, res) {
+    // repairmanModel.findAll({
+    //   where: {
+    //     [or]: [
+    //       {username: {
+    //         [Op.like]: req.query
+    //       }}
+    //     ]
+    //   }
+    // })
+    res.send(req.query);
   }
 }
 
--- routes/repairman-route.js
+++ routes/repairman-route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const repairman = require('../controllers/repairman-controller');
 
 router.get('/', repairman.findAll);
+router.post('/', repairman.findSome);
 
 module.exports = router;
