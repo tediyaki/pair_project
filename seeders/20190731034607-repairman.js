ed122a03a0ee90af9d98632df60e54017a6c41de
havus <havus.it@gmail.com>
2019-08-02 00:56:04 +0700

    a

--- seeders/20190731034607-repairman.js
+++ seeders/20190731034607-repairman.js
@@ -6,49 +6,49 @@ module.exports = {
       username: "johndoe",
       specialist: "window",
       cost: 20000,
-      avatar: "img/ava/1.png",
+      photo: "img/ava/1.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "jokodarmin",
       specialist: "door",
       cost: 300000,
-      avatar: "img/ava/3.png",
+      photo: "img/ava/3.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "selabrustanoit",
       specialist: "floor",
       cost: 120000,
-      avatar: "img/ava/10.png",
+      photo: "img/ava/10.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "stark",
       specialist: "roof",
       cost: 50000,
-      avatar: "img/ava/2.png",
+      photo: "img/ava/2.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "supaijo",
       specialist: "door",
       cost: 350000,
-      avatar: "img/ava/4/png",
+      photo: "img/ava/4/png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "sugriwok",
       specialist: "floor",
       cost: 125000,
-      avatar: "img/ava/5.png",
+      photo: "img/ava/5.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: "mpokdarmi",
       specialist: "floor",
       cost: 110000,
-      avatar: "img/ava/6.png",
+      photo: "img/ava/6.png",
       createdAt: new Date(),
       updatedAt: new Date()
     }])
