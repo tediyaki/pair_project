fab658804e5f14df289b07a86aac67c9b3fa5e2c
havus <havus.it@gmail.com>
2019-08-01 21:10:40 +0700

    ..

--- helper/countDate.js
+++ helper/countDate.js
@@ -8,5 +8,5 @@ function countDate(str, days) {
   return moment(moment(str).add(days,'days')).fromNow();
 };
 
-console.log(countDate('2019-07-22 07:00:00', 30));
+// console.log(countDate('2019-07-22 07:00:00', 30));
 module.exports = countDate;
